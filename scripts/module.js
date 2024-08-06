88fd588a6ed94e0064ed51f8f8c925772ef79503
Luca Gioppo <gioppoluca@libero.it>
2024-08-06 18:47:49 +0200

    feature: initial commit

--- scripts/module.js
+++ scripts/module.js
@@ -2,6 +2,31 @@ Hooks.once('init', async function() {
 
 });
 
+
+
 Hooks.once('ready', async function() {
 
+});
+
+
+// Add button to Actor Sheet for opening app
+Hooks.on("getJournalPageSheetHeaderButtons", (sheet, buttons) => {
+	console.log("check the sheet")
+	console.log(sheet)
+	if (true) {
+		buttons.unshift({
+			label: "Timeline data",
+			class: "export-pdf",
+			icon: "fas fa-timeline",
+			onclick: () => {
+				// Open Config window
+				//new SheetExportconfig(sheet.actor, sheetType, sheet).render(true);
+
+				// Bring window to top
+				// Object.values(ui.windows)
+				// 	.filter(window => window instanceof SheetExportconfig)[0]
+				// 	?.bringToTop();
+			},
+		});
+	}
 });
