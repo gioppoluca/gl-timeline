917f0577ae57d58a17d0d96a0748c701b2d63838
Luca Gioppo <gioppoluca@libero.it>
2024-08-10 12:45:41 +0200

    feature: mockup working, applicationV2 probably working

---
+++ scripts/graph_dashboard.js
@@ -0,0 +1,191 @@
+//import GraphHandlebarsHelpers from "./graph_handlebarHandlers.js";
+//import GraphForm from "./graph_form.js";
+const { ApplicationV2, HandlebarsApplicationMixin } = foundry.applications.api
+
+export default class GraphDashboard extends HandlebarsApplicationMixin(ApplicationV2) {
+  constructor(options = {}) {
+    super(options);
+    console.log('constuctor of GraphDashboard')
+  }
+
+  static DEFAULT_OPTIONS = {
+    id: "gl-timetable",
+    form: {
+      handler: GraphDashboard.myFormHandler,
+      closeOnSubmit: true,
+    },
+    position: {
+      width: 640,
+      height: "auto",
+    },
+    tag: "div", // The default is "div"
+    window: {
+      icon: "fas fa-gear", // You can now add an icon to the header
+      title: "FOO.form.title"
+    }
+
+  }
+  static async myFormHandler(event, form, formData) {
+    // Do things with the returned FormData
+    console.log('formhandler');
+    console.log(form);
+  }
+  get title() {
+    console.log('getTitle')
+    //return `My Module: ${game.i18n.localize(this.options.window.title)}`;
+    return 'gioppo';
+  }
+  _onRender(context, options) {
+    this.svg_orig();
+  }
+
+  static PARTS = {
+    form: {
+      template: "modules/gl-timeline/scripts/templates/graph_dashboard.html"
+    }
+  }
+
+  // this is the new getData
+  _prepareContext(options) {
+    const setting = null; //game.settings.get("foo", "config");
+    console.log('_prepareContext');
+    return {
+      setting
+    }
+  }
+
+
+  svg_orig() {
+    const stacks = ['stack1', 'stack2', 'stack3'];
+
+    // Data with custom labels, optional end dates, and colors
+    const data = [
+      { start: new Date('2023-05-23'), end: new Date('2023-05-24'), stack: 'stack1', value: 'data1', labelStart: 'Alpha_start', labelEnd: 'Alpha_end', color: 'steelblue' },
+      { start: new Date('2023-06-24'), stack: 'stack1', value: 'data2', labelStart: 'Beta_start', labelEnd: null, color: 'orange' }, // Single event
+      { start: new Date('2023-06-23'), end: new Date('2023-06-25'), stack: 'stack2', value: 'dataA', labelStart: 'Gamma_start', labelEnd: 'Gamma_end', color: 'green' },
+      { start: new Date('2023-05-24'), stack: 'stack2', value: 'dataB', labelStart: 'Delta_start', labelEnd: null, color: 'purple' }, // Single event
+      { start: new Date('2023-07-01'), end: new Date('2023-07-02'), stack: 'stack3', value: 'dataC', labelStart: 'Epsilon_start', labelEnd: 'Epsilon_end', color: 'red' }
+    ];
+
+    // Extract the unique dates from the data
+    const customTicks = [...new Set(data.flatMap(d => [d.start.getTime(), d.end ? d.end.getTime() : d.start.getTime()]))]
+      .sort((a, b) => a - b);
+
+    const margin = { top: 20, right: 20, bottom: 50, left: 50 },
+      width = 800 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+    // Scales
+    const x = window.d3.scaleBand()
+      .domain(customTicks)
+      .range([0, width])
+      .padding(0.0);
+
+    const y = window.d3.scaleBand()
+      .domain(stacks)
+      .range([0, height])
+      .padding(0.2);
+
+    const barHeight = 7; // Set the height of bars and circles
+
+    // Axis
+    const xAxis = window.d3.axisBottom(x)
+      .tickFormat(d => {
+        const item = data.find(e => e.start.getTime() === d || (e.end && e.end.getTime() === d));
+        return item ? (item.start.getTime() === d ? item.labelStart : item.labelEnd) : '';
+      });
+
+    const yAxis = window.d3.axisLeft(y);
+
+    // SVG Container
+    const svg = window.d3.select("#mygraph").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Add a thin black line for each stack
+    svg.selectAll(".stack-line")
+      .data(stacks)
+      .enter().append("line")
+      .attr("class", "stack-line")
+      .attr("x1", 0)
+      .attr("x2", width)
+      .attr("y1", d => y(d) + y.bandwidth() / 2)
+      .attr("y2", d => y(d) + y.bandwidth() / 2);
+
+    // Bars for stacks with both start and end dates
+    svg.selectAll(".bar")
+      .data(data.filter(d => d.end))
+      .enter().append("rect")
+      .attr("class", d => `bar ${d.stack}`)
+      .attr("x", d => x(d.start.getTime()) + (x.bandwidth() - (x.bandwidth() * x.padding())) / 2) // Align bars with tick marks
+      .attr("y", d => y(d.stack) + (y.bandwidth() - barHeight) / 2)  // Center the bar vertically
+      .attr("width", d => {
+        const startX = x(d.start.getTime()) + (x.bandwidth() - (x.bandwidth() * x.padding())) / 2;
+        const endX = x(d.end.getTime()) + (x.bandwidth() - (x.bandwidth() * x.padding())) / 2;
+        return endX - startX; // Correct width calculation
+      })
+      .attr("height", barHeight)  // Set bar height to 7 pixels
+      .attr("fill", d => d.color);
+
+    // Circles for single-point events
+    svg.selectAll(".circle-event")
+      .data(data.filter(d => !d.end))
+      .enter().append("circle")
+      .attr("class", "circle-event")
+      .attr("cx", d => x(d.start.getTime()) + x.bandwidth() / 2) // Center the circle horizontally at the tick
+      .attr("cy", d => y(d.stack) + y.bandwidth() / 2)  // Center the circle vertically
+      .attr("r", barHeight / 2)  // Set radius to 3.5 pixels (half of bar height)
+      .attr("fill", d => d.color)
+      .style("stroke", "black")  // Add a thin black line for each circle
+      .style("stroke-width", "1px");
+
+    // Add the X Axis
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", `translate(0,${height})`)
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "start") // Align text to the start
+      .attr("transform", "rotate(45)") // Rotate labels by 45 degrees
+      .attr("dx", "-0.4em") // Adjust x position
+      .attr("dy", "0.55em"); // Adjust y position
+
+    // Add the Y Axis
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+    // Function to download the SVG as an image
+    // function downloadImage() {
+    //   const svgElement = window.d3.select("svg").node();
+    //   const serializer = new XMLSerializer();
+    //   const source = serializer.serializeToString(svgElement);
+
+    //   const image = new Image();
+    //   image.src = 'data:image/svg+xml;base64,' + btoa(source);
+
+    //   const canvas = document.createElement("canvas");
+    //   canvas.width = svgElement.width.baseVal.value;
+    //   canvas.height = svgElement.height.baseVal.value;
+    //   const context = canvas.getContext("2d");
+
+    //   image.onload = function () {
+    //     context.drawImage(image, 0, 0);
+    //     const link = document.createElement("a");
+    //     link.download = "timeline.png";
+    //     link.href = canvas.toDataURL("image/png");
+    //     link.click();
+    //   };
+    // }
+
+    // Add a right-click event to save the SVG
+    window.d3.select("svg").on("contextmenu", function (event) {
+      event.preventDefault();
+//      downloadImage();
+    });
+
+  }
+
+}
--- scripts/module.js
+++ scripts/module.js
@@ -1,10 +1,13 @@
-Hooks.once('init', async function() {
+import GraphDashboard from "./graph_dashboard.js"
+
+
+Hooks.once('init', async function () {
 
 });
 
 
 
-Hooks.once('ready', async function() {
+Hooks.once('ready', async function () {
 
 });
 
@@ -16,17 +19,68 @@ Hooks.on("getJournalPageSheetHeaderButtons", (sheet, buttons) => {
 	if (true) {
 		buttons.unshift({
 			label: "Timeline data",
-			class: "export-pdf",
+			class: "timeline",
 			icon: "fas fa-timeline",
 			onclick: () => {
+				console.log('invoking the dashboard');
+				new GraphDashboard();
+
 				// Open Config window
 				//new SheetExportconfig(sheet.actor, sheetType, sheet).render(true);
 
 				// Bring window to top
-				// Object.values(ui.windows)
-				// 	.filter(window => window instanceof SheetExportconfig)[0]
-				// 	?.bringToTop();
+				Object.values(ui.windows)
+					.filter(window => window instanceof GraphDashboard)[0]
+					?.bringToTop();
 			},
 		});
 	}
+});
+
+// Hooks.on("renderJournalDirectory", (app, html, data) => {
+// 	console.log("check the directory")
+// 	console.log(app)
+// 	console.log(html)
+// 	html.find(".directory-item").each((i, item) => {
+// 		let $item = $(item);
+// 		let $button = $("<button class='timeline'>Timeline</button>");
+// 		$item.append($button);
+// 	});
+// });
+
+
+Hooks.on("getSceneControlButtons", (controls) => {
+	console.log("check the controls")
+	console.log(controls)
+	controls.filter(control => control.name === "notes")[0].tools.push({
+		name: "timeline",
+		title: "Timeline",
+		button: true,
+		icon: "fas fa-timeline",
+		onClick: () => {
+			// Open Config window
+			//new SheetExportconfig(sheet.actor, sheetType, sheet).render(true);
+			console.log('invoking the dashboard');
+			var gd = new GraphDashboard();
+			console.log('----------- clicked button - created - now render')
+			console.log(gd)
+			gd.render(true);
+			// Open Config window
+			//new SheetExportconfig(sheet.actor, sheetType, sheet).render(true);
+
+			// Bring window to top
+			 Object.values(ui.windows)
+			 	.filter(window => window instanceof GraphDashboard)[0]
+			 	?.bringToTop();
+		}
+	});
+	// controls.push({
+	// 	name: "timeline",
+	// 	title: "Timeline",
+	// 	icon: "fas fa-timeline",
+	// 	onclick: () => {
+	// 		// Open Config window
+	// 		//new SheetExportconfig(sheet.actor, sheetType, sheet).render(true);
+	// 	}
+	// });
 });
